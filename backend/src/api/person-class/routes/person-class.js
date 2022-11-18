7cb5ff76ab36dd96f9d7bc3b096ad3c7866e20b7
Zach Stevenson <stevezac@oregonstate.edu>
2022-11-18 01:15:12 -0500

    Adding additional structure to backend.

---
+++ backend/src/api/person-class/routes/person-class.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * person-class router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::person-class.person-class');
