64010a6b84a01c8db8a59d6bdd1b567e02d42b0e
Ann Marie Hicks <hicksa2@oregonstate.edu>
2022-11-30 17:06:18 -0500

    fixed linting errors

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,46 +1,45 @@
 const Footer = () => {
   return (
 
-<footer class="text-center text-lg-start bg-dark text-muted">
-  <section class="">
-    <div class="container text-white text-center text-md-start pt-3 mt-4">
-      <div class="row mt-3">
-        <div class="col-md-1 col-lg-3 col-xl-3 mx-auto mb-3">
-        <img className='w-75' src='/footerlogo.png' />
-        </div>
+    <footer class='text-center text-lg-start bg-dark text-muted'>
+      <section class=''>
+        <div class='container text-white text-center text-md-start pt-3 mt-4'>
+          <div class='row mt-3'>
+            <div class='col-md-1 col-lg-3 col-xl-3 mx-auto mb-3'>
+              <img className='w-75' src='/footerlogo.png' />
+            </div>
 
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-3">Address</h6>
-          <p>
-          1500 SW Chandler Avenue<br></br>
-          Bend, Oregon 97702
-          </p>
-        </div>
+            <div class='col-md-4 col-lg-3 col-xl-3 mx-auto mb-4'>
+              <h6 class='text-uppercase fw-bold mb-3'>Address</h6>
+              <p>
+                1500 SW Chandler Avenue<br />
+                Bend, Oregon 97702
+              </p>
+            </div>
 
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-3">
-          <h6 class="text-uppercase fw-bold mb-3">Contact</h6>
-          <p>donnellp@oregonstate.edu</p>
-          <p>(514) 706 2071</p>
-        </div>
+            <div class='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-3'>
+              <h6 class='text-uppercase fw-bold mb-3'>Contact</h6>
+              <p>donnellp@oregonstate.edu</p>
+              <p>(514) 706 2071</p>
+            </div>
 
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-3">
-          <h6 class="text-uppercase fw-bold mb-2">Links</h6>
-          <ul class="list-group">
-            <li style={{ color: '#D73F09' }} class="list-group-item bg-dark pb-0 border-0"><a href="https://oregonstate.edu/" class="text-reset">Oregon State University</a></li>
-            <li style={{ color: '#D73F09' }} class="list-group-item bg-dark pb-0 border-0"><a href="https://eecs.oregonstate.edu/" class="text-reset">Oregon State-Cascades</a></li>
-            <li style={{ color: '#D73F09' }} class="list-group-item bg-dark border-0"><a href="https://eecs.oregonstate.edu/" class="text-reset">School of EECS</a></li>
-          </ul>
+            <div class='col-md-4 col-lg-3 col-xl-3 mx-auto mb-3'>
+              <h6 class='text-uppercase fw-bold mb-2'>Links</h6>
+              <ul class='list-group'>
+                <li style={{ color: '#D73F09' }} class='list-group-item bg-dark pb-0 border-0'><a href='https://oregonstate.edu/' class='text-reset'>Oregon State University</a></li>
+                <li style={{ color: '#D73F09' }} class='list-group-item bg-dark pb-0 border-0'><a href='https://eecs.oregonstate.edu/' class='text-reset'>Oregon State-Cascades</a></li>
+                <li style={{ color: '#D73F09' }} class='list-group-item bg-dark border-0'><a href='https://eecs.oregonstate.edu/' class='text-reset'>School of EECS</a></li>
+              </ul>
+            </div>
+
+          </div>
         </div>
+      </section>
+
+      <div class='text-center pt-2 pb-4'>
+        © 2021 Copyright: 2020-2022 Soundbendor Lab - All Rights Reserved
       </div>
-
-    </div>
-  </section>
-
-  <div class="text-center pt-2 pb-4">
-    © 2021 Copyright: 2020-2022 Soundbendor Lab - All Rights Reserved
-  </div>
-
-</footer>
+    </footer>
   )
 }
 
--- frontend/pages/apply.js
+++ frontend/pages/apply.js
@@ -2,48 +2,48 @@
 const Apply = () => {
   return (
     <div>
-    <div className='container-fluid px-0'>
-    <div style={{ backgroundColor: '#D73F09' }} className='row py-5'>
-      <div className='col'>
-        <div className='container'>
+      <div className='container-fluid px-0'>
+        <div style={{ backgroundColor: '#D73F09' }} className='row py-5'>
+          <div className='col'>
+            <div className='container'>
               <div className='text-center text-white'>
                 <h1>Join Soundbendor!</h1>
                 <p>Soundbendor lab is actively recruiting students with interdisciplinary backgrounds in music and computer science who wish to combine these interests through graduate studies in artificial intelligence at Oregon State University.</p>
                 <h5><b>Applications Due</b></h5>
-                <ul id="applyul" class="list-group">
-                <li style={{ backgroundColor: '#D73F09' }} class="list-group-item border-0 text-white"><b>December 1, 2022 (Ph.D)</b></li>
-                <li style={{ backgroundColor: '#D73F09' }} class="list-group-item border-0 text-white"><b>January 1, 2023 (M.S.)</b></li>
-                <li style={{ backgroundColor: '#D73F09' }} class="list-group-item border-0 text-white"><b>February 1, 2023 (B.S.)</b></li>
+                <ul id='applyul' class='list-group'>
+                  <li style={{ backgroundColor: '#D73F09' }} class='list-group-item border-0 text-white'><b>December 1, 2022 (Ph.D)</b></li>
+                  <li style={{ backgroundColor: '#D73F09' }} class='list-group-item border-0 text-white'><b>January 1, 2023 (M.S.)</b></li>
+                  <li style={{ backgroundColor: '#D73F09' }} class='list-group-item border-0 text-white'><b>February 1, 2023 (B.S.)</b></li>
                 </ul>
-                <a class="btn btn-dark mt-3" href="https://eecs.oregonstate.edu/graduate-programs-applying-for-admission" role="button">Apply Now</a>
+                <a class='btn btn-dark mt-3' href='https://eecs.oregonstate.edu/graduate-programs-applying-for-admission' role='button'>Apply Now</a>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='row justify-content-center py-5'>
+          <div className='col-md-4'>
+            <div className='container'>
+              <div className='text-center'>
+                <h4>Request Information</h4>
+                <form>
+                  <div class='form-group'>
+                    <label for='exampleInputPassword1'>Full Name</label>
+                    <input type='name' class='form-control' id='inputName' placeholder='Full Name' />
+                  </div>
+                  <div class='form-group m-1'>
+                    <label for='exampleInputEmail1'>Email Address</label>
+                    <input type='email' class='form-control' id='inputEmail1' aria-describedby='emailHelp' placeholder='Enter email' />
+                  </div>
+
+                  <button type='submit' class='btn btn-dark mt-3'>Request Information</button>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-    <div className='row justify-content-center py-5'>
-    <div className='col-md-4'>
-        <div className='container'>
-      <div className="text-center">
-      <h4>Request Information</h4>
-<form>
-<div class="form-group">
-    <label for="exampleInputPassword1">Full Name</label>
-    <input type="name" class="form-control" id="inputName" placeholder="Full Name"/>
-  </div>
-  <div class="form-group m-1">
-    <label for="exampleInputEmail1">Email Address</label>
-    <input type="email" class="form-control" id="inputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-  </div>
-
-  <button type="submit" class="btn btn-dark mt-3">Request Information</button>
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
   )
 }
 
