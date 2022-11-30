8639f7e619accd0ba02be3b88f9f628430f64897
nawaji <jonathant15191@gmail.com>
2022-11-30 03:34:27 -0800

    Basic search and filter bar
    Basic implementation of them, still needs styling, spacing, etc

--- frontend/pages/projects.js
+++ frontend/pages/projects.js
@@ -1,6 +1,12 @@
 const Projects = () => {
   return (
-    <h1>Projects</h1>
+    <>
+      <div className='row py-5'>
+        <div className='text-center'>
+          <h1>Projects</h1>
+        </div>
+      </div>
+    </>
   )
 }
 
--- frontend/pages/team.js
+++ frontend/pages/team.js
@@ -1,6 +1,40 @@
+import style from '../styles/Team.module.css'
+
 const Team = () => {
   return (
-    <h1>Team</h1>
+    <>
+      <div className='row py-5'>
+        <div className='text-center'>
+          <h1>Team Members</h1>
+        </div>
+        <div>
+          <ul className={style.ul} >
+              <div className='text-center'>
+                <li className={style.li}>
+                  <input type='text' id='person-search-input' placeholder='Search...'></input>
+                </li>
+                <li className={style.li}>
+                  <i className='search-icon'>[SEARCH ICON PH]</i>
+                </li>
+                <li className={style.li}>
+                <select className='dropdown-select'>
+                  <option value='Default'>Filter</option>
+                  <option value='Class'>Class</option>
+                  <option value='Degree'>Degree</option>
+                  <option value='Group'>Group</option>
+                </select>
+                </li>
+              </div>
+          </ul>
+        </div>
+      </div>
+
+      <div className='row bg-light py-5'>
+        <div className='col'>
+          test
+        </div>
+      </div>
+    </>
   )
 }
 
