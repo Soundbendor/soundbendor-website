7a6a3dc4fb8f52d31e917eb12dc0e3c207f4d0cf
Zach Stevenson <stevezac@oregonstate.edu>
2023-02-02 17:53:07 -0500

    removed duplicates according to the linting script

--- frontend/components/Personcard.js
+++ frontend/components/Personcard.js
@@ -20,31 +20,4 @@ const PersonCard = ({ person }) => {
   )
 }
 
-const PersonCardPlaceholder = ({ person, image }) => {
-  return (
-    <article className={style.card}>
-      <div className={style.image}>
-        <svg
-          className='bd-placeholder-img bd-placeholder-img-lg'
-          width='240'
-          height='240'
-          xmlns={person.Photo}
-          role='img'
-          preserveAspectRatio='xMidYMid slice'
-          focusable='false'
-        >
-          <title>Placeholder</title>
-          <rect width='100%' height='100%' fill='grey' />
-          <text x='50%' y='50%' fill='white' dy='.3em' dominantBaseline='middle' textAnchor='middle'>{person.LastName}</text>
-        </svg>
-      </div>
-      <div className={style.bottom}>
-        <h5><strong>{person.FirstName} {person.LastName}</strong></h5>
-        <hr className={style.horizontalbar} />
-        <h6 className='beaverorange'>{person.person_class}</h6>
-      </div>
-    </article>
-  )
-}
-
-export { PersonCard, PersonCardPlaceholder }
+export { PersonCard }
