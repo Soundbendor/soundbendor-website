fe052048b53bac2ccdd87a7c8df3ef56e93953fc
Ann Marie Hicks <hicksa2@oregonstate.edu>
2023-02-08 15:54:18 -0500

    fixed title color to global beaverorange

--- frontend/components/Personcard.js
+++ frontend/components/Personcard.js
@@ -14,7 +14,7 @@ const PersonCard = ({ person }) => {
       <div className={style.bottom}>
         <h5><strong>{person.FirstName} {person.LastName}</strong></h5>
         <hr className={style.horizontalbar} />
-        <h6 className='beaverorange'>{person.personClass.name}</h6>
+        <h6 className='beaverorange'>{person.personClass.Name}</h6>
       </div>
     </article>
   )
