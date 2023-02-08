727ea6b71be9c29e2856ae060e4e5556cea58212
Ann Marie Hicks <hicksa2@oregonstate.edu>
2023-02-08 15:59:10 -0500

    linting

--- frontend/components/Projectcard.js
+++ frontend/components/Projectcard.js
@@ -3,7 +3,7 @@ import style from '../styles/Projectcard.module.css'
 const ProjectCard = ({ project }) => {
   return (
     <article className={style.card}>
-      <div class="embed-responsive embed-responsive-16by9">
+      <div class='embed-responsive embed-responsive-16by9'>
         <iframe class='embed-responsive-item' src={project.projectLink} allowFullScreen> </iframe>
       </div>
       <div className={style.bottom}>
