55f4c643825857e50be5b416159be8addc8008a6
nawaji <jonathant15191@gmail.com>
2023-02-12 20:12:17 -0800

    Search handler implementation
    Implemented the search handler for team page, also removed backup pages that I used for reference.

--- frontend/components/Personcard.js
+++ frontend/components/Personcard.js
@@ -4,7 +4,7 @@ const PersonCard = ({ person }) => {
       <div className='card'>
         <img src={person.photoImage.url} className='card-img-top' alt='Team member portrait'></img>
         <div className='card-body'>
-          <h5 className='card-title'>{person.FirstName} {person.LastName}</h5>
+          <h5 className='card-title'>{person.formattedPersonName}</h5>
           <h6 className='card-subtitle mb-2 beaverorange'>{person.personClass.Name}</h6>
         </div>
       </div>
--- frontend/models/people.js
+++ frontend/models/people.js
@@ -8,9 +8,27 @@ function Person (rawData) {
   p.RawData = rawData
   p.photoImage = ImageService.getImage({ id__eq: p.Photo })
   p.personClass = PersonClassService.getPersonClass({ id__eq: p.person_class })
+  p.RawData.formattedPersonName = p.RawData.FirstName + " " + p.RawData.LastName
+  p.RawData.personClass = p.personClass.Name
+
   return p
 }
 
-const PersonService = BaseService.constructDefaultService('api::person.person', 'person', 'people', Person)
+const peopleFilterFunctions = Object.assign({}, BaseService.filterFunctions)
+peopleFilterFunctions.searchNameAndClass = (key, value, obj) => obj.formattedPersonName.includes(value) || obj.FirstName.includes(value) ||obj.personClass.includes(value)
+
+const PersonService = {
+  getRawPeople: BaseService.getRawData('api::person.person', peopleFilterFunctions),
+  getRawPerson: function (kwargs) {
+    return BaseService.getRawDatum(PersonService.getRawPeople, kwargs)
+  },
+  getPerson: function (kwargs) {
+    return BaseService.getDatum(PersonService.getRawPerson, Person, kwargs)
+  },
+  getPeople: function (kwargs) {
+    return BaseService.getData(PersonService.getRawPeople, Person, kwargs)
+  }
+}
+//const PersonService = BaseService.constructDefaultService('api::person.person', 'person', 'people', Person)
 
 module.exports = PersonService
