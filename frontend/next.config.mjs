315bcd594efbdf4ecf70c10000166a07c5024585
Zach Stevenson <stevezac@oregonstate.edu>
2023-02-16 23:57:39 -0500

    Getting the command line to stop providing warnings for __nextDefaultLocale and __nextLocale

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 import BaseService from './models/__base.js'
 
+const buildPathMapQuery = (urlPath) => {
+  return {__nextDefaultLocale: 'en-US', __nextLocale: 'en-US', path: urlPath}
+}
+
 const nextConfig = {
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
   trailingSlash: true,
   reactStrictMode: true,
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
@@ -12,7 +20,7 @@ const nextConfig = {
     if (dev) suffix = '/'
     for (const page of pages) {
       const pageType = page.PageType.toLowerCase()
-      pathMap[page.URLPath] = { page: suffix + pageType, query: { path: page.URLPath } }
+      pathMap[page.URLPath] = { page: suffix + pageType, query: buildPathMapQuery(page.URLPath) }
     }
     return pathMap
   }
