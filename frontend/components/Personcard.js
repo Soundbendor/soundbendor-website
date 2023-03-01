341beffa0cb002b29793620dbc72cb158938ec4c
nawaji <jonathant15191@gmail.com>
2023-02-28 22:56:46 -0800

    Adds links to projects/publications
    Each card has a dropdown menu with page links to projects or publications.

--- frontend/components/Personcard.js
+++ frontend/components/Personcard.js
@@ -8,6 +8,15 @@ const PersonCard = ({ person }) => {
         <div className='card-body'>
           <h5 className='card-title beaverorange'>{person.formattedPersonName}</h5>
           <p className='card-subtitle mb-2 text-muted'>{person.personClass.Name}</p>
+          <div class='btn-group-vertical' role='group' aria-label='project link'>
+            <button type='button' className='btn btn-dark dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
+              [PH] Links
+            </button>
+            <ul className='dropdown-menu'>
+              <li><a className='dropdown-item' id='item-projects' href='/projects/'>Projects</a></li>
+              <li><a className='dropdown-item' id='item-publications' href='/publications/'>Publications</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
