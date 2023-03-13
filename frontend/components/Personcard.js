8fce157a19e7677e14a35faf079f42b535e1fa4a
Ann Marie Hicks <hicksa2@oregonstate.edu>
2023-03-13 15:14:12 -0400

    fixed name length changing the card size/layout

--- frontend/components/Personcard.js
+++ frontend/components/Personcard.js
@@ -1,13 +1,13 @@
 const PersonCard = ({ person }) => {
   return (
     <div className='col'>
-      <div className='card'>
+      <div className='card h-100'>
         <div className='ratio ratio-1x1'>
           <img src={person.photoImage.url} className='card-img-top' alt='Team member portrait' />
         </div>
-        <div className='card-body'>
+        <div className='card-body d-flex flex-column'>
           <h5 className='card-title beaverorange'>{person.formattedPersonName}</h5>
-          <p className='card-subtitle mb-2 text-muted'>{person.personClass.Name}</p>
+          <p className='card-subtitle mb-2 text-muted mt-auto'>{person.personClass.Name}</p>
         </div>
       </div>
     </div>
