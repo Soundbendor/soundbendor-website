a3b6e74a3189c075614d3a9466aa7eabc9fd5576
Zach Stevenson <stevezac@oregonstate.edu>
2023-03-13 01:06:12 -0400

    Adding a success message to the apply page and removing the no linting directives

--- frontend/pages/apply.js
+++ frontend/pages/apply.js
@@ -1,12 +1,12 @@
+import $ from 'jquery'
+
 const Apply = (data) => {
   const ContactFormSubmit = async (event) => {
     event.preventDefault()
-    /* eslint-disable no-unused-vars */
-    /* eslint-disable no-undef */
-    jQuery('#requestInfoSubmit').prop('disabled', true).addClass('disabled')
-    const jqxhr = jQuery.ajax({
+    $('#requestInfoSubmit').prop('disabled', true).addClass('disabled')
+    $.ajax({
       url: data.get_info_url,
-      data: jQuery(event.target).serialize(),
+      data: $(event.target).serialize(),
       xhrFields: {
         withCredentials: true
       },
@@ -14,45 +14,36 @@ const Apply = (data) => {
       type: 'POST',
       dataType: 'json'
     }).always((response) => {
-      jQuery('#requestInfoSubmit').prop('disabled', false).removeClass('disabled')
-      /* eslint-disable no-undef */
-      /* eslint-disable no-unused-vars */
+      const contents = [
+        '<div class="alert alert-success alert-dismissible" role="alert">',
+        'Your request has been sent!',
+        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+      ]
+      $('#form-alert-container').html(contents.join(''))
+      $(event.target)[0].reset()
+      $('#requestInfoSubmit').prop('disabled', false).removeClass('disabled')
     })
   }
 
   return (
     <>
-      <div className='row justify-content-center py-5'>
-        <div className='col-lg-6'>
-          <div className='container'>
-            <div className='text-center'>
-              <h4>Request Information</h4>
-              <div className='embed-responsive embed-responsive-16by9'>
-                <form name='contact-form' action={data.get_info_url} onSubmit={ContactFormSubmit}>
-                  <input type='hidden' name='submit' value='submit' />
-                  <div className='form-group'>
-                    <div className='row-mb3'>
-
-                      <label htmlFor='inputName'>Full Name</label>
-                      <div className='col'>
-                        <input type='name' name={data.get_info_name} className='form-control' id='inputName' placeholder='Full Name' />
-                      </div>
-                    </div>
-                  </div>
-                  <div className='form-group'>
-                    <label htmlFor='inputEmail1'>Email Address</label>
-                    <input type='email' name={data.get_info_email} className='form-control' id='inputEmail1' placeholder='Enter email' />
-                  </div>
-                  <div className='form-group'>
-                    <label htmlFor='inputComment1'>Comments</label>
-                    <textarea name={data.get_info_comment} className='form-control' id='inputComment1' placeholder='Comments' />
-                  </div>
-
-                  <button type='submit' className='btn btn-dark mt-3' id='requestInfoSubmit'>Request Information</button>
-                </form>
-              </div>
+      <div className='row justify-content-center text-center py-5'>
+        <div className='col-10 col-md-8 col-lg-6 col-xl-4 col-xxl-3'>
+          <h4>Request Information</h4>
+          <form name='contact-form' action={data.get_info_url} onSubmit={ContactFormSubmit}>
+            <div id='form-alert-container' />
+            <input type='hidden' name='submit' value='submit' />
+            <div className='form-group'>
+              <label htmlFor='inputName'>Full Name</label>
+              <input type='name' name={data.get_info_name} className='form-control' id='inputName' placeholder='Full Name' />
+              <label htmlFor='inputEmail1'>Email Address</label>
+              <input type='email' name={data.get_info_email} className='form-control' id='inputEmail1' placeholder='Enter email' />
+              <label htmlFor='inputComment1'>Comments</label>
+              <textarea name={data.get_info_comment} className='form-control' id='inputComment1' placeholder='Comments' />
             </div>
-          </div>
+            <button type='submit' className='btn btn-dark mt-3' id='requestInfoSubmit'>Request Information</button>
+          </form>
         </div>
       </div>
     </>
