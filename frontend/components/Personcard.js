713de9bf705c07f36f2eb44cabb5c5a365b9df9c
Ann Marie Hicks <hicksa2@oregonstate.edu>
2023-04-30 16:41:50 -0400

    Added card flip with projects/publications buttons

--- frontend/components/Personcard.js
+++ frontend/components/Personcard.js
@@ -1,13 +1,22 @@
 const PersonCard = ({ person }) => {
   return (
     <div className='col'>
-      <div className='card h-100'>
-        <div className='ratio ratio-1x1'>
-          <img src={person.photoImage.url} className='card-img-top' alt='Team member portrait' />
+      <div className='card card-flip h-100'>
+        <div className='card-front d-flex flex-column'>
+          <div className='ratio ratio-1x1'>
+            <img src={person.photoImage.url} className='card-img-top' alt='Team member portrait' />
+          </div>
+          <div className='card-body d-flex flex-column'>
+            <h5 className='card-title beaverorange'>{person.formattedPersonName}</h5>
+            <p className='card-subtitle mb-2 text-muted mt-auto'>{person.personClass.Name}</p>
+          </div>
         </div>
-        <div className='card-body d-flex flex-column'>
-          <h5 className='card-title beaverorange'>{person.formattedPersonName}</h5>
-          <p className='card-subtitle mb-2 text-muted mt-auto'>{person.personClass.Name}</p>
+        <div className='card-back d-flex flex-column'>
+          <div className='card-body d-flex flex-column'>
+            <p className='card-subtitle mb-2 text-muted mt-auto'>About Me?</p>
+            <button className='btn btn-link mt-auto'>Projects</button>
+            <button className='btn btn-link mt-1'>Publications</button>
+          </div>
         </div>
       </div>
     </div>
