a2c4263027862930be705bcc243f30eefd5d9d0e
Zach Stevenson <stevezac@oregonstate.edu>
2023-05-09 00:20:02 -0400

    this is adding the ability for the system to do the necessary revalidating of the data on the site once it has been published

---
+++ backend/src/plugins/link-me/admin/src/pages/HomePage/index.js
@@ -0,0 +1,20 @@
+/*
+ *
+ * HomePage
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+import pluginId from '../../pluginId';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Transfer SoundBendor Data to Website</h1>
+      <p><a href="http://beta.SoundBendor.org/api/import/">Start the Transfer</a></p>
+    </div>
+  );
+};
+
+export default HomePage;
