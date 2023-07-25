7d8f966038605b0e714e29200ad73663b51603ad
Micah <stalberm@oregonstate.edu>
2023-07-25 11:26:40 -0700

    Adds initial project type filtering

---
+++ frontend/models/project-types.js
@@ -0,0 +1,5 @@
+import BaseService from './__base'
+
+const ProjectTypeService = BaseService.constructDefaultService('api::project-target-type.project-target-type', 'projectType', 'projectTypes')
+
+module.exports = ProjectTypeService
--- frontend/pages/projects.js
+++ frontend/pages/projects.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ProjectCard } from '../components/Projectcard'
 import ProjectService from '../models/projects'
+import ProjectTypeService from '../models/project-types'
 
 const createProjectListDisplay = (projects) => {
   if (projects.length) {
@@ -19,23 +20,37 @@ const createProjectYearListDisplay = (projectYears) => {
   )
 }
 
+const createProjectTypeListDisplay = (projectTypes) => {
+  return projectTypes.map((type) =>
+    <option key={type.Name} value={type.id}>{type.Name}</option>
+  )
+}
+
 const Projects = () => {
   const presortFilter = { presortBy: 'InitialPublishedDate', presortDirection: -1 }
   const projects = ProjectService.getProjects(presortFilter)
   const projectSearchPlaceholder = '(e.g., ' + projects[0].Name + ')'
   const [projectListDisplay, setProjectListDisplay] = useState(createProjectListDisplay(projects))
   const projectYearListDisplay = createProjectYearListDisplay(ProjectService.getProjectYears())
+  const projectTypeListDisplay = createProjectTypeListDisplay(ProjectTypeService.getProjectTypes())
 
   const searchHandler = async (event) => {
     event.preventDefault()
     const searchField = document.getElementById('project-search')
     const yearField = document.getElementById('project-year')
+    const typeField = document.getElementById('project-type')
+    
     const filters = presortFilter
     if (searchField.value) {
       filters.x__searchNameAndDescription = searchField.value
     }
     if (yearField.value) {
       filters.InitialPublishedDate__sw = yearField.value
+    }
+    if (typeField.value) {
+      if (typeField.value) {
+        filters.project_target_type__eq = parseInt(typeField.value);
+      }
     }
     setProjectListDisplay(createProjectListDisplay(ProjectService.getProjects(filters)))
   }
@@ -53,15 +68,22 @@ const Projects = () => {
       </div>
       <div className='container'>
         <div className='row justify-content-between'>
-          <div className='col-sm-8'>
+          <div className='col-sm-6'>
             <label htmlFor='project-search' className='form-label'>Search by Name</label>
             <input type='search' className='form-control' id='project-search' onChange={searchHandler} placeholder={projectSearchPlaceholder} />
           </div>
-          <div className='col-sm-4'>
+          <div className='col-sm-3'>
             <label htmlFor='project-year' className='form-label'>Year Published</label>
             <select className='form-select' id='project-year' onChange={searchHandler}>
               <option defaultValue value=''>All Years</option>
               {projectYearListDisplay}
+            </select>
+          </div>
+          <div className='col-sm-3'>
+            <label htmlFor='project-type' className='form-label'>Project Type</label>
+            <select className='form-select' id='project-type' onChange={searchHandler}>
+              <option defaultValue value=''>All Types</option>
+              {projectTypeListDisplay}
             </select>
           </div>
         </div>
